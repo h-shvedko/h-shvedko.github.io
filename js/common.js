103376e377c10fe14092c88ad752087b446c5d4b
h-shvedko <hennadii.shvedko@copernicus.org>
2018-01-10 14:10:21 +0100

    Added preloader  script

--- js/common.js
+++ js/common.js
@@ -1,14 +1,22 @@
-$(document).click(function (e) {
-    var container = $("#wohnungen");
-    var elem = $(".grid-second-menu");
+$(document).ready(function () {
+    setTimeout(hideElement(), 20000);
 
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if(!elem.is(e.target) && elem.has(e.target).length === 0){
-            elem.css("visibility", "hidden");
-            elem.css("opacity", 0);
+    $(document).click(function (e) {
+        var container = $("#wohnungen");
+        var elem = $(".grid-second-menu");
+
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if(!elem.is(e.target) && elem.has(e.target).length === 0){
+                elem.css("visibility", "hidden");
+                elem.css("opacity", 0);
+            }
+        } else {
+            elem.css("visibility", "visible");
+            elem.css("opacity", 1);
         }
-    } else {
-        elem.css("visibility", "visible");
-        elem.css("opacity", 1);
-    }
+    });
 });
+
+function hideElement(){
+    $(".preloader").hide();
+}

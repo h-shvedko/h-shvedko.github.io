0f81e2c43b46653e510b80971f7f6b703de1c708
h-shvedko <hennadii.shvedko@copernicus.org>
2018-01-10 12:54:22 +0100

    Added second menu for flats in main navigation

--- js/common.js
+++ js/common.js
@@ -1,5 +1,14 @@
-$(function() {
+$(document).click(function (e) {
+    var container = $("#wohnungen");
+    var elem = $(".grid-second-menu");
 
-	// Custom JS
-
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if(!elem.is(e.target) && elem.has(e.target).length === 0){
+            elem.css("visibility", "hidden");
+            elem.css("opacity", 0);
+        }
+    } else {
+        elem.css("visibility", "visible");
+        elem.css("opacity", 1);
+    }
 });

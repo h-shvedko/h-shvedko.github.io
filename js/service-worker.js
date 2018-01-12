bf3024e8494fe5cfd7176e9aef981f6826e1d1a1
h-shvedko <hennadii.shvedko@copernicus.org>
2018-01-12 12:41:27 +0100

    service-worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -2,31 +2,32 @@ var cacheName = 'GR';
 var filesToCache = [
     '/',
     '../index.html',
+    '../fonts/FontAwesome.woff',
     '../img/main_background.jpeg',
     '../img/main_wohnung1.jpeg',
     '../img/main_wohnung2.jpeg',
     '../img/main_wohnung3.jpeg',
     '../img/main_wohnung4.jpeg',
-    '../img/map_main.jpeg',
+    '../img/map_main.png',
     '../manifest.json',
     '../css/main.min.css',
     '../js/scripts.min.js'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(caches.open(cacheName)
-        .then(function(cache) {
+        .then(function (cache) {
             return cache.addAll(filesToCache)
-                .then(function() {
+                .then(function () {
                     self.skipWaiting();
                 });
         }));
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     e.waitUntil(caches.keys()
-        .then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
+        .then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
                 if (key !== cacheName)
                     return caches.delete(key);
             }));
@@ -34,10 +35,19 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(event.request).then(function() {
-            return fetch(event.request);
+self.addEventListener('fetch', function (e) {
+    e.respondWith(caches.match(e.request)
+        .then(function (response) {
+            return response || fetch(e.request)
+                .then(function (resp) {
+                    return caches.open(cacheName)
+                        .then(function (cache) {
+                            cache.put(e.request, resp.clone());
+                            return resp;
+                        })
+                }).catch(function (event) {
+                    console.log('Error fetching data!');
+                })
         })
     );
 });

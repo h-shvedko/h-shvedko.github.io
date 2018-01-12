144dfef790e2a9136f149c5c8c42f5b96bf6c558
h-shvedko <hennadii.shvedko@copernicus.org>
2018-01-12 12:54:44 +0100

    service-worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -14,20 +14,20 @@ var filesToCache = [
     '../js/scripts.min.js'
 ];
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
     e.waitUntil(caches.open(cacheName)
-        .then(function (cache) {
+        .then(function(cache) {
             return cache.addAll(filesToCache)
-                .then(function () {
+                .then(function() {
                     self.skipWaiting();
                 });
         }));
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
     e.waitUntil(caches.keys()
-        .then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
+        .then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
                 if (key !== cacheName)
                     return caches.delete(key);
             }));
@@ -35,17 +35,17 @@ self.addEventListener('activate', function (e) {
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
     e.respondWith(caches.match(e.request)
-        .then(function (response) {
+        .then(function(response) {
             return response || fetch(e.request)
-                .then(function (resp) {
+                .then(function (resp){
                     return caches.open(cacheName)
-                        .then(function (cache) {
+                        .then(function(cache){
                             cache.put(e.request, resp.clone());
                             return resp;
                         })
-                }).catch(function (event) {
+                }).catch(function(event){
                     console.log('Error fetching data!');
                 })
         })

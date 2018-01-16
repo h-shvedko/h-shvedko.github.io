8c03a10b4dd99763e04e7330c2afe1db18deb207
h-shvedko <hennadii.shvedko@copernicus.org>
2018-01-16 16:49:01 +0100

    added flat's pages

--- js/common.js
+++ js/common.js
@@ -1,12 +1,9 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.min.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
-}
+// if ('serviceWorker' in navigator) {
+// navigator.serviceWorker.register('/service-worker.min.js').then(function(registration) {
+// }).catch(function(err) {
+//     console.log('ServiceWorker registration failed: ', err);
+// });
+// }
 
 $(document).ready(function () {
 
@@ -15,7 +12,7 @@ $(document).ready(function () {
         var elem = $(".grid-second-menu");
 
         if (!container.is(e.target) && container.has(e.target).length === 0) {
-            if(!elem.is(e.target) && elem.has(e.target).length === 0){
+            if (!elem.is(e.target) && elem.has(e.target).length === 0) {
                 elem.css("visibility", "hidden");
                 elem.css("opacity", 0);
             }
@@ -31,14 +28,38 @@ $(document).ready(function () {
         }, 1000);
     });
 
+    $('#anfahrt').click(function () {
+        $("html").animate({
+            scrollTop: $(document).height() - $('.display-main-4')[0].scrollHeight
+        }, 1000);
+    });
+
     $('.grid-arrow-down i').click(function () {
         $("html").animate({
             scrollTop: $('.display-main-3')[0].scrollHeight
         }, 1000);
     });
+
+    $('.grid-arrow-down-wohnung i').click(function () {
+        $("html").animate({
+            scrollTop: $('.display-main-5')[0].scrollHeight
+        }, 1000);
+    });
+
+    $('.photo-over').click(function (e) {
+        var name = $(this).parent().attr('data-name');
+        $('.gallery').css('visibility', 'visible');
+        $('.gallery .overlay').css('opacity', 0.8);
+        $('.gallery>div>img').attr('src', '/img/' + name + '_page.jpeg');
+    });
+
+    $('#close, .gallery>.overlay').click(function () {
+        $('.gallery .overlay').css('opacity', 0);
+        $('.gallery').css('visibility', 'hidden');
+    });
 });
 
-window.onload = function() {
+window.onload = function () {
     $(".preloader").hide();
-    $('.map iframe').attr('src', 'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJy0ZcLb_UpEcRiakz-DaIkq8&key=AIzaSyD6CHCQODTf_Xp8WF1BOxTBmvU8inT4Q60');
+    $('.map iframe').attr('src', 'https://www.google.com/maps/embed/v1/place?q=place:Goettingen&key=AIzaSyD6CHCQODTf_Xp8WF1BOxTBmvU8inT4Q60');
 }
